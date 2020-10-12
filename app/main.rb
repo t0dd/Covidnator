0efd83d25f2a74ed1235e2eeabc9e42a11cd12a8
Todd Mahoney <euphoricsoulrave@gmail.com>
2020-10-11 19:39:38 -0600

    adding some direction and a new sprite

--- app/main.rb
+++ app/main.rb
@@ -8,6 +8,7 @@ class CovidnatorGame
     state.player.x ||= W/2
     state.player.y ||= 24
     state.player.speed ||= 3
+    state.player.direction ||= 0
   end
 
   def tick
@@ -17,15 +18,19 @@ class CovidnatorGame
   end
 
   def render_background 
-    outputs.solids << [0,0,W,H, 130,130,130]
+    outputs.solids << [0,0,W,H, 117,132,155]
+  end
+  def render_pence
+    outputs.sprites << [0, 320, 550, 400, 'sprites/pence.png']
   end
 
   def render_player
-    outputs.sprites << [state.player.x, state.player.y, 128/5, 71/4, 'sprites/player.png']
+    outputs.sprites << [state.player.x, state.player.y, 623/20, 658/20, 'sprites/player.png', state.player.direction]
   end
 
   def render 
     render_background 
+    render_pence
     render_player
   end
 
@@ -36,15 +41,19 @@ class CovidnatorGame
   def process_inputs_game
     # keyboard input -------------------------
     if inputs.keyboard.key_held.right
-      state.player.x += 6
+      state.player.x += state.player.speed
+      state.player.direction = 270
     elsif args.inputs.keyboard.key_held.left
-      state.player.x -= 6
+      state.player.x -= state.player.speed
+      state.player.direction = 90
     end
 
     if inputs.keyboard.key_held.up
       state.player.y += state.player.speed
+      state.player.direction = 0
     elsif args.inputs.keyboard.key_held.down
       state.player.y -= state.player.speed
+      state.player.direction = 180
     end
   end
 end
